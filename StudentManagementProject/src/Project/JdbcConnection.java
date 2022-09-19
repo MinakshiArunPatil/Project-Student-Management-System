07ccd243730c9936079764598e820ce73968ad1f
MinakshiArunPatil <112397020+MinakshiArunPatil@users.noreply.github.com>
2022-09-20 00:08:50 +0530

    Add files via upload

---
+++ StudentManagementProject/src/Project/JdbcConnection.java
@@ -0,0 +1,36 @@
+package Project;
+/*import java.io.*;
+import java.sql.*;
+ 
+
+public class JdbcConnection {
+	
+	class GFG {
+	    public static void main(String[] args) throws Exception
+	    {
+	        String url
+	            = "jdbc:mysql://localhost:3306/studentdata"; // table details
+	        String username = "rootgfg"; // MySQL credentials
+	        String password = "gfg123";
+	        String query
+	            = "select *from students"; // query to be run
+	        Class.forName(
+	            "com.mysql.cj.jdbc.Driver"); // Driver name
+	        Connection con = DriverManager.getConnection(
+	            url, username, password);
+	        System.out.println(
+	            "Connection Established successfully");
+	        Statement st = con.createStatement();
+	        ResultSet rs
+	            = st.executeQuery(query); // Execute query
+	        rs.next();
+	        String name
+	            = rs.getString("name"); // Retrieve name from db
+	 
+	        System.out.println(name); // Print result on console
+	        st.close(); // close statement
+	        con.close(); // close connection
+	        System.out.println("Connection Closed....");
+	    }
+	}
+}*/
